ce098e5e8d74fda3d2f504b157d006affbb8f031
Axel van 't Westeinde <axel.westeinde@gmail.com>
2020-11-03 14:32:46 +0100

    Improved watering process and exit handling

--- auto_watering.py
+++ auto_watering.py
@@ -1,29 +1,42 @@
 import RPi.GPIO as gpio
 import serial
-from time import sleep
+import time
+import os
 
-gpio.setmode(gpio.BOARD)
-gpio.setup(16, gpio.OUT, initial=gpio.HIGH)
-gpio.output(16, 1)
+def main():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(16, gpio.OUT, initial=gpio.HIGH)
 
-for i in range(0,10):
+    for i in range(0,10):
+        try:
+            port = serial.Serial(f'/dev/ttyACM{i}')
+            print(f'Current port is ACM{i}')
+        except:
+            print(f'Port {i} is unsuitable')
+
+    def give_water():
+        gpio.output(16, 0)
+        time.sleep(1)
+        gpio.output(16, 1)
+
+    elapsed = 0
+    start = time.time()
+    while True:
+        line = port.readline()
+        print(line)
+        value = float(line[-6:-2])
+        elapsed = time.time() - start
+        if value > 725 & elapsed > 50: # Higher value means less moisture
+            give_water()
+            elapsed = 0
+            start = time.time()
+
+
+if __name__ == '__main__':
     try:
-        port = serial.Serial(f'/dev/ttyACM{i}')
-        print(f'Current port is ACM{i}')
-    except:
-        print(f'Port {i} is unsuitable')
-
-def give_water():
-    gpio.output(16, 0)
-    sleep(1)
-    gpio.output(16, 1)
-    sleep(50)
-
-
-while True:
-    line = port.readline()
-    print(line)
-    value = float(line[-6:-2])
-    if value > 725: # Higher value means less moisture
-        give_water()
-
+        gpio.cleanup()
+        main()
+    except KeyboardInterrupt:
+        gpio.cleanup()
+    # except:
+    #     os.system('sudo reboot')
