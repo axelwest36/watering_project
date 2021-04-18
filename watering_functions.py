942432041ab1bd45ad2ffef6e4b17765e5201eda
Axel van ´t Westeinde <axelwest@Axels-MacBook-Pro.local>
2021-04-18 10:06:14 +0200

    fixed type of query result

--- watering_functions.py
+++ watering_functions.py
@@ -64,7 +64,7 @@ def get_moisture_level():
     c.execute("""
     SELECT moisture FROM moisture_data ORDER BY currentdate DESC, currenttime DESC LIMIT 1;
     """)
-    latest_moisture = " ".join(c.fetchone())
+    latest_moisture = str(c.fetchone())
     conn.close()
     return latest_moisture
 
